4f0c2dd255d4ab238f330afcd50aaa2de399f102
Anup Mohan <anup.mohan@kdi.kongsberg.com>
2021-01-03 18:49:26 +0530

    Added app header module

---
+++ Angular/digital-book/src/app/header/header-routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { HeaderHomeComponent } from './header-home/header-home.component';
+
+export const routes: Routes = [{
+  path: '',
+  component: HeaderHomeComponent
+}];
